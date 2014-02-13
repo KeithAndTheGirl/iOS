ad046202aa0b451ed5936c7aed1da3bcc3f2ba58
Nicolas Rostov <NicolasRostov@gmail.com>
2014-02-14 01:45:25 +0700

    Episode list minor UI changes

--- KATG/UI/Custom Views/KATGShowView.m
+++ KATG/UI/Custom Views/KATGShowView.m
@@ -48,7 +48,7 @@
     NSArray *images = [[show valueForKeyPath:@"Guests.picture_url"] allObjects];
     for(int i=0; i<[images count]; i++) {
         UIImageView *imgView = [[UIImageView alloc] initWithFrame:
-                                CGRectMake(16+i*56, 60, 50, 50)];
+                                CGRectMake(16+i*56, 64, 50, 50)];
         [self.contentView addSubview:imgView];
         imgView.tag = 111;
         imgView.contentMode = UIViewContentModeScaleAspectFit;
